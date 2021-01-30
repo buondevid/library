5d028e0ea112a2240592980a4ffd355a559a423a
buondevid <davidebriano@gmail.com>
2021-01-30 08:02:13 +0100

    add localStorage, fix bugs, polish CSS

--- scripts.js
+++ scripts.js
@@ -7,16 +7,26 @@ const abort = document.querySelector('.red');
 let myLibrary = [{
 	title: 'Harry Potter',
 	author: 'J. K. Rowling',
-	pages: 300,
+	pages: 3407,
 	read: true,
 },
 {
 	title: 'Lord of the Rings',
-	author: 'Tolkien',
-	pages: 1232,
+	author: 'J. R. R. Tolkien',
+	pages: 1137,
 	read: false,
 },
 ];
+
+function populateStorage() {
+	localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+}
+
+function setLibrary() {
+	myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+}
+
+if (localStorage.length !== 0) setLibrary();
 
 function Book(title, author, pages, read) {
 	this.title = title;
@@ -28,6 +38,7 @@ function Book(title, author, pages, read) {
 	};
 }
 
+// function to populate (creating elements) the library with myLibrary objects
 function showBook() {
 	for (const item of myLibrary) {
 		const bookBackground = document.createElement('div');
@@ -40,7 +51,7 @@ function showBook() {
 		deleteBook.type = 'button';
 		deleteBook.className = 'delete-book';
 		deleteBook.textContent = 'X';
-		
+
 		const bookTitle = document.createElement('h2');
 		bookTitle.textContent = item.title;
 
@@ -59,8 +70,16 @@ function showBook() {
 		checkRead.checked = item.read === true;
 		checkRead.classList.add('checkboxRead');
 		checkRead.checked && bookBackground.classList.add('read');
-		checkRead.addEventListener('change', () => bookBackground.classList.toggle('read'));
-		
+		// this event listener dynamically change the object myLibrary when changing Read status on homepage
+		checkRead.addEventListener('change', () => {
+			bookBackground.classList.toggle('read');
+			for (const obj of myLibrary) {
+				if (obj.title === bookTitle.textContent)  {
+					obj.read = checkRead.checked;
+					populateStorage();
+				}
+			}
+		});
 		bookInfo.appendChild(deleteBook);
 		bookInfo.appendChild(bookTitle);
 		bookInfo.appendChild(bookAuthor);
@@ -69,10 +88,12 @@ function showBook() {
 		labelCheck.appendChild(checkRead);
 		bookBackground.appendChild(bookInfo);
 		library.appendChild(bookBackground);
-		deleteBook.addEventListener('click', removeBook(deleteBook.nextSibling.textContent));
+		deleteBook.addEventListener('click', removeBook(bookTitle.textContent));
 	}
+	populateStorage();
 }
 
+// add book to Object myLibrary
 function addBookToLibrary() {
 	const title = document.getElementById('form-title').value;
 	const author = document.getElementById('form-author').value;
@@ -92,9 +113,11 @@ function removeBook(bookTitle) {
 	};
 }
 
-// header button opens the book form and resets it, if re-clicked
+// event listeners
+
 newBookButton.addEventListener('click', () => {
 	library.classList.add('hidden');
+	document.querySelector('.signature').classList.add('hidden');
 	formBackground.classList.add('full-scale');
 	form.reset();
 });
@@ -108,7 +131,13 @@ form.addEventListener('submit', (e) => {
 abort.addEventListener('click', () => {
 	form.reset();
 	formBackground.classList.remove('full-scale');
-	setTimeout(() => library.classList.remove('hidden'), 300);
+	setTimeout(() => {
+		library.classList.remove('hidden');
+		document.querySelector('.signature').classList.remove('hidden');
+	},
+	300);
 });
+
+// populate library at start
 
 showBook();
